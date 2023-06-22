8461a87e056758b7542953eba0c4067f33aa1002
sydorova-nataliya <nsidorova200363@gmail.com>
2023-06-22 23:25:30 +0300

    added form with statement

--- dist/js/jquery.main.js
+++ dist/js/jquery.main.js
@@ -11,12 +11,35 @@ $( document ).ready(function() {
     $('.navigation__btn').click(function(e){
         $('.navigation').toggleClass('active');
     })
+    $('#state-btn').click(function(e){
+        $('.state').addClass('active');
+        $('.state').removeClass('hidden');
+        $('.header').addClass('blur');
+        $('.main').addClass('blur');
+    })
+    $('.state__close').click(function(e){
+        $('.state').removeClass('active');
+        $('.state').addClass('hidden');
+        $('.header').removeClass('blur');
+        $('.main').removeClass('blur');
+    })
 
-    $('.slider').slick({
-        dots: true,
-        infinite: true,
-        arrows: false,
-        adaptiveHeight: true
-      });
+    jQuery.validator.addMethod("checkMask", function(value, element) {
+        return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value); 
+   });
+   
+   $('form').validate({
+     rules: {
+       ph: {
+         checkMask: true
+       }
+     },
+     messages: {
+       ph: {
+         checkMask: "Введите полный номер телефона"
+       }
+     }
+   });
+   $('.phone').mask("+7(999)999-9999", {autoclear: false});
 });
 
